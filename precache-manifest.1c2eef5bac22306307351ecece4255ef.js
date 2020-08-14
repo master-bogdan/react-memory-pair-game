deea24df130b51b115135efdcadd389f5bbf0f89
masterbogdan <maiskiychavk@gmail.com>
2020-08-14 20:37:14 +0300

    Updates

---
+++ precache-manifest.1c2eef5bac22306307351ecece4255ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b4cc29e93f2b515ac31db9146a6fdb",
+    "url": "/react-memory-pair-game/index.html"
+  },
+  {
+    "revision": "70568d26873a0946a15c",
+    "url": "/react-memory-pair-game/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d38d4b753bfdc484e6b5",
+    "url": "/react-memory-pair-game/static/css/main.8f8326c5.chunk.css"
+  },
+  {
+    "revision": "70568d26873a0946a15c",
+    "url": "/react-memory-pair-game/static/js/2.ac961790.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-memory-pair-game/static/js/2.ac961790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38d4b753bfdc484e6b5",
+    "url": "/react-memory-pair-game/static/js/main.a9442f25.chunk.js"
+  },
+  {
+    "revision": "8874b07defc2b5c5212a",
+    "url": "/react-memory-pair-game/static/js/runtime-main.7b619fbb.js"
+  },
+  {
+    "revision": "0f26e9152f692ca68d3559a4c8fb5407",
+    "url": "/react-memory-pair-game/static/media/1.0f26e915.jpg"
+  },
+  {
+    "revision": "9b49cf599ab73d40545a39c30381a2ea",
+    "url": "/react-memory-pair-game/static/media/2.9b49cf59.jpg"
+  },
+  {
+    "revision": "f05cd15591e9f45522cae6fa8b075b14",
+    "url": "/react-memory-pair-game/static/media/3.f05cd155.jpg"
+  },
+  {
+    "revision": "20c2f6606f5fcf49929903300eea7f67",
+    "url": "/react-memory-pair-game/static/media/4.20c2f660.jpg"
+  },
+  {
+    "revision": "56b0e2c12bf7fb6a6fdab620e7743155",
+    "url": "/react-memory-pair-game/static/media/5.56b0e2c1.jpg"
+  },
+  {
+    "revision": "912475710b186d35489e71d17db97b77",
+    "url": "/react-memory-pair-game/static/media/6.91247571.jpg"
+  },
+  {
+    "revision": "fd24ad4b2bcff6556cf831bf60c8f58e",
+    "url": "/react-memory-pair-game/static/media/card.fd24ad4b.jpg"
+  }
+]);
